c5bd7cbadb2240392bc07a5814deb07bbc738690
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2024-10-25 13:40:11 -0400

    Allocate more memory for graphdb and add update script

---
+++ update.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Check if an argument is passed
+if [ -z "$1" ]; then
+  echo "Error: No argument provided. Please provide one of the following: 'taxonomy', 'cwl', or 'query'."
+  exit 1
+fi
+
+# Set DOCKER_IMAGE and DOCKER_SERVICE based on the input
+case "$1" in
+  "taxonomy")
+    DOCKER_IMAGE="webgme/taxonomy:latest"
+    DOCKER_SERVICE="taxonomy"
+    ;;
+  "cwl")
+    DOCKER_IMAGE="webgme/cwl:latest"
+    DOCKER_SERVICE="cwl"
+    ;;
+  "query")
+    DOCKER_IMAGE="git.isis.vanderbilt.edu:5050/meijerop/taxonomy-query:latest"
+    DOCKER_SERVICE="taxonomy-query"
+    ;;
+  *)
+    echo "Error: Invalid input. Please provide one of the following: 'taxonomy', 'cwl', or 'query'."
+    exit 1
+    ;;
+esac
+
+# cd /udcp/
+
+# Execute Docker commands
+echo "Pulling Docker image: $DOCKER_IMAGE"
+docker pull "$DOCKER_IMAGE"
+
+echo "Starting Docker service: $DOCKER_SERVICE"
+docker compose up -d --build "$DOCKER_SERVICE"
